7a5ba8de15d14533267d7f18213b4e009991a6a3
DavisO <99267654+DavisMoney@users.noreply.github.com>
2022-02-28 09:25:06 -0500

    fix grammer

--- Simple solvers/DO.c
+++ Simple solvers/DO.c
@@ -1,7 +1,8 @@
 #include <math.h>
 #include <stdio.h>
 int main() {
-//input a, b ,c
+
+//input a, b ,c / variables
     double a, b, c, discriminant;
     printf("Enter the A value:");
     scanf("%lf", &a);
@@ -11,16 +12,18 @@ int main() {
     scanf("%lf", &c);
     printf("\n");
     discriminant = (b * b - 4 * a * c);
-//Quadratic eqation way    
+
+//Quadratic variables  
     double root1, root2;
-    
+
+//Using the Quadratic equation way
     if (discriminant > 0) {
         root1 = (-b + sqrt(discriminant)) / (2 * a);
         root2 = (-b - sqrt(discriminant)) / (2 * a);
         ("root1 = %lf", root1);
         ("root2 = %lf", root2);
     }
-//Print roots from Quadratic
+//Print roots from Quadratic equation
     else if (discriminant == 0) {
         root1 = root2 = -b / (2 * a);
         printf("root1 = root2 = %lf; \n", root1);
@@ -28,15 +31,15 @@ int main() {
         printf("The Quadratic equation will take the inputs a, b, c and put them into the equation (b * b - 4 * a * c) to give us our discriminant.\n");
         printf("It will then all go into the equation (-b +/- sqrt(discriminant)) / (2 * a)) to give us our roots.\n\n");
         printf("Using the quadratic formula the discriminant and roots are: \n");
+
         printf("The discriminant is: %lf \n", discriminant);
         printf("root1 = %lf  \n", root1);
         printf("root2 = %lf  \n\n", root2);
-
-
+        
 //New way variables
     double root_1, root_2, m, z, discriminant2;
          
-//New way 
+//Using the New way 
         m = (-b / (2 * a));
         discriminant2 = (m * m - (c / a));
         z = sqrt(discriminant2);
@@ -48,6 +51,7 @@ int main() {
         printf("Once we have m we sub m, a, and c into (m * m - (c / a)) to give us our discriminant.\n");
         printf("Once we have the discriminant we will square it to give us z. We will then sub m and z into (m +/- z) to give us our two roots.\n\n");
         printf("Using the new method the discriminant and roots are: \n");
+
         printf("The discriminant is: %lf \n", discriminant2);
         printf("root_1 = %lf \n", root_1);
         printf("root_2 = %lf", root_2);
