bd59f282638287b8abd2f6722ca8e323d0f806c5
Masterblaster436 <99211959+Masterblaster436@users.noreply.github.com>
2022-02-28 10:01:17 -0500

    Explained More

--- Simple solvers/RM.py
+++ Simple solvers/RM.py
@@ -19,12 +19,12 @@ distanceFromRoot2=averageOfRoots-root2QuadraticFormula # The distance from the o
 root1NewMethod=(averageOfRoots)-(distanceFromRoot) # This is the first answer given by the new method 
 root2NewMethod=(averageOfRoots)-(distanceFromRoot2) # This is the second answer given by the new method (If there is a second root)
 if (discriminant==0)or(discriminant>0):
-    if (root1QuadraticFormula==root1NewMethod): #To make sure that the quadratic formula and new method give the same result for the first root
-        answer1=round(root1QuadraticFormula, 2) #root1QuadraticFormula rounded to two decimal places
-        print(answer1) 
+    if (root1QuadraticFormula==root1NewMethod): # To make sure that the quadratic formula and new method give the same result for the first root
+        answer1=round(root1QuadraticFormula, 2) # root1QuadraticFormula rounded to two decimal places
+        print(answer1) # The final first answer
 if (discriminant>0):
-    if (root2QuadraticFormula==root2NewMethod): #To make sure that the quadratic formula and new method give the same result for the second root
-        answer2=round(root2QuadraticFormula, 2) #root2QuadraticFormula rounded to two decimal places
-        print(answer2)
+    if (root2QuadraticFormula==root2NewMethod): # To make sure that the quadratic formula and new method give the same result for the second root
+        answer2=round(root2QuadraticFormula, 2) # root2QuadraticFormula rounded to two decimal places
+        print(answer2) # The final second answer
 if (discriminant==0) or (discriminant>0):
     print('Solved using the quadratic formula and new method')
