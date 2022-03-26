077cdc475438e2689237730a9bb700e18305e5ea
LucL <levesqueluc87@gmail.com>
2022-03-26 18:50:39 -0400

    changed var to const where necessary

--- Games/LLEGSB/script.js
+++ Games/LLEGSB/script.js
@@ -76,8 +76,8 @@ function question1(){
   pointsText.text = '';
   playerPoints = 0;
   quitText.text = '';
-  var rndmNum1 = Math.floor((Math.random() * 50) + 1);
-  var rndmNum2 = Math.floor((Math.random() * 1000) + 200);
+  const rndmNum1 = Math.floor((Math.random() * 50) + 1);
+  const rndmNum2 = Math.floor((Math.random() * 1000) + 200);
   questionText.text = (`Write an albebraic expression for the following verbal expression: The sum of ${rndmNum2} and ${rndmNum1} times a number(x).`);
   //defines the correct answer for the question
   correctAnswer = `${rndmNum1}x+${rndmNum2}`;
@@ -90,8 +90,8 @@ function question2(){
   confirmationText.text = '';
   quitText.text = '';
   hintText.text = '';
-  var rndmNum1 = Math.floor(Math.random() * (22 - 12 + 1)) + 12;
-  var power = 2;
+  const rndmNum1 = Math.floor(Math.random() * (22 - 12 + 1)) + 12;
+  const power = 2;
   var answr = Math.pow(rndmNum1, power)
   questionText.text = `Which of the following values of x is a solution of the equation given: x^2 = ${answr}`;
   correctAnswer = `${rndmNum1}`;
@@ -104,13 +104,13 @@ function question3(){
   confirmationText.text = '';
   quitText.text = '';
   hintText.text = ''
-  var rndmNum1 =  Math.floor(Math.random() * (20 - 2 + 1)) + 2;
-  var rndmNum2 =  Math.floor(Math.random() * (20 - 2 + 1)) + 2;
-  var rndmNum3 =  Math.floor(Math.random() * (20 - 2 + 1)) + 2;
-  var expression = `${rndmNum1}(${rndmNum2}+${rndmNum3}x)`
+  const rndmNum1 =  Math.floor(Math.random() * (20 - 2 + 1)) + 2;
+  const rndmNum2 =  Math.floor(Math.random() * (20 - 2 + 1)) + 2;
+  const rndmNum3 =  Math.floor(Math.random() * (20 - 2 + 1)) + 2;
+  const expression = `${rndmNum1}(${rndmNum2}+${rndmNum3}x)`
   questionText.text = `Find the answer using the distributive property; ${expression}`;
-  var rndmNum1times2 = (rndmNum1*rndmNum2);
-  var rndmNum1times3 = (rndmNum1*rndmNum3);
+  const rndmNum1times2 = (rndmNum1*rndmNum2);
+  const rndmNum1times3 = (rndmNum1*rndmNum3);
   correctAnswer = `${rndmNum1times2}+${rndmNum1times3}x`
 }
 // question 4 function
@@ -120,11 +120,11 @@ function question4(){
   confirmationText.text = '';
   quitText.text = '';
   hintText.text = ''
-  var rndmNum1 = Math.floor(Math.random() * (-20 - 20 + 1)) + 20;
-  var rndmNum2 = Math.floor(Math.random() * (10 - 2 + 1)) + 2;
-  var rndmNum3 = Math.floor(Math.random() * (7 - 2 + 1)) + 2;
-  var rndmNum4 = Math.floor(Math.random() * (-20 - 20 + 1)) + 20;
-  var rndmNum5 = Math.floor(Math.random() * (10 - 2 + 1)) + 2;
+  const rndmNum1 = Math.floor(Math.random() * (-20 - 20 + 1)) + 20;
+  const rndmNum2 = Math.floor(Math.random() * (10 - 2 + 1)) + 2;
+  const rndmNum3 = Math.floor(Math.random() * (7 - 2 + 1)) + 2;
+  const rndmNum4 = Math.floor(Math.random() * (-20 - 20 + 1)) + 20;
+  const rndmNum5 = Math.floor(Math.random() * (10 - 2 + 1)) + 2;
   questionText.text = `Combine all like terms in the given expression: ${rndmNum1}x + ${rndmNum2}y + ${rndmNum4}x - ${rndmNum3}z + ${rndmNum5}y` 
   correctAnswer = `${rndmNum1+rndmNum4}x+${rndmNum2+rndmNum5}y-${rndmNum3}z`;
 }
@@ -136,12 +136,12 @@ function question5(){
   confirmationText.text = '';
   quitText.text = '';
   hintText.text = ''
-  var y1 = Math.floor(Math.random() * (200 - 1 + 1)) + 1;
-  var y2 = Math.floor(Math.random() * (200 - 1 + 1)) + 1;
-  var x1 = Math.floor(Math.random() * (200 - 1 + 1)) + 1;
-  var x2 = Math.floor(Math.random() * (200 - 1 + 1)) + 1;
+  const y1 = Math.floor(Math.random() * (200 - 1 + 1)) + 1;
+  const y2 = Math.floor(Math.random() * (200 - 1 + 1)) + 1;
+  const x1 = Math.floor(Math.random() * (200 - 1 + 1)) + 1;
+  const x2 = Math.floor(Math.random() * (200 - 1 + 1)) + 1;
   questionText.text = `What is the slope of a line(to 2 decimal places) that contain the points (${x1}, ${y1}) and (${x2}, ${y2}).`;
-  var slopeEquation = ((y2-y1)/(x2-x1));
+  const slopeEquation = ((y2-y1)/(x2-x1));
   slopeRounded = slopeEquation.toFixed(2);
   correctAnswer = `${slopeRounded}`;
 }
@@ -152,17 +152,17 @@ function question6(){
   confirmationText.text = '';
   quitText.text = '';
   hintText.text = ''
-  var x1 = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
-  var y1 = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
-  var x2 = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
-  var y2 = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
+  const x1 = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
+  const y1 = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
+  const x2 = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
+  const y2 = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
   questionText.text = `What is the length of a line segment(to 2 decimal places) with the endpoints (${x1}, ${y1}) and (${x2}, ${y2})`  
-  var x = (x2-x1); 
-  var y = (y2-y2);
-  var xSquare = (x**2);
-  var ySquare = (y**2);
-  var add = (xSquare+ySquare);
-  var squareRoot = (add**0.5);
+  const x = (x2-x1); 
+  const y = (y2-y2);
+  const xSquare = (x**2);
+  const ySquare = (y**2);
+  const add = (xSquare+ySquare);
+  const squareRoot = (add**0.5);
   ansRounded = squareRoot.toFixed(2);
   correctAnswer = ansRounded;
   
@@ -176,10 +176,10 @@ function question7(){
   confirmationText.text = '';
   quitText.text = '';
   hintText.text = '';
-  var exp = 0;
-  var rndmNum = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
+  const exp = 0;
+  const rndmNum = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
   questionText.text = `Solve: ${rndmNum}^${exp}`;
-  var answr = `${rndmNum**exp}`;
+  const answr = `${rndmNum**exp}`;
   correctAnswer = answr;
 }
 // question 8 function
@@ -189,14 +189,14 @@ function question8(){
   confirmationText.text = '';
   quitText.text = '';
   hintText.text = '';
-  var rndmNum1 = Math.floor(Math.random() * (10 - 2 + 1)) + 10;
-  var rndmNum2 = Math.floor(Math.random() * (10 - 2 + 1)) + 10;
-  var expression = `(x+${rndmNum1})(x+${rndmNum2})`;
+  const rndmNum1 = Math.floor(Math.random() * (10 - 2 + 1)) + 10;
+  const rndmNum2 = Math.floor(Math.random() * (10 - 2 + 1)) + 10;
+  const expression = `(x+${rndmNum1})(x+${rndmNum2})`;
   questionText.text = `Expand the expression ${expression}`;
-  var ans1 = `x^2`;
-  var ans2 = `${rndmNum1+rndmNum2}x`;
-  var ans3 = `${rndmNum1*rndmNum2}`;
-  var finalAns = `${ans1}+${ans2}+${ans3}`;
+  const ans1 = `x^2`;
+  const ans2 = `${rndmNum1+rndmNum2}x`;
+  const ans3 = `${rndmNum1*rndmNum2}`;
+  const finalAns = `${ans1}+${ans2}+${ans3}`;
   correctAnswer = finalAns;
 }
 
@@ -207,15 +207,15 @@ function question9(){
   confirmationText.text = '';
   quitText.text = '';
   hintText.text = '';
-  var a = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
-  var b = Math.floor(Math.random() * (10 - 6 + 1)) + 6;
-  var c = Math.floor(Math.random() * (4 - 2 + 1)) + 2;
+  const a = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
+  const b = Math.floor(Math.random() * (10 - 6 + 1)) + 6;
+  const c = Math.floor(Math.random() * (4 - 2 + 1)) + 2;
   questionText.text = `Find the solutions(to 2 decimal places) of the equation ${a}x^2+${b}x+${c} using the quadratic formula. (Write as; #,# with the smallest number first.)`
   const discriminant = (b**2-4*a*c);
   const quadFormula1 = (-b+(discriminant)**0.5) / (2*a);
   const quadFormula2 = (-b-(discriminant)**0.5) / (2*a);
-  var quad1Rounded = quadFormula1.toFixed(2);
-  var quad2Rounded = quadFormula2.toFixed(2);
+  const quad1Rounded = quadFormula1.toFixed(2);
+  const quad2Rounded = quadFormula2.toFixed(2);
   correctAnswer = `${quad1Rounded},${quad2Rounded}`;
 }
 function checkAnswer(answer){
@@ -266,4 +266,5 @@ function quitGame(){
   hintText.text = '';
 }
 //a timer to quit the game after 3 minutes
-setTimeout(quitGame, 180000)
+const minutes = 180000;
+setTimeout(quitGame, minutes)
